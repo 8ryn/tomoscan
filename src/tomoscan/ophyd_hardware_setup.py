e0bb050972e2e13453d7ab1f9391ff061d7c6373
Bryn Roberts <bar@observatorysciences.co.uk>
2024-02-29 15:30:01 +0000

    Addition of demo notebook

--- src/tomoscan/ophyd_hardware_setup.py
+++ src/tomoscan/ophyd_hardware_setup.py
@@ -23,8 +23,7 @@ from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
 from ophyd.areadetector.plugins import HDF5Plugin_V34
 
 
-class MyHDF5Plugin(FileStoreHDF5IterativeWrite, HDF5Plugin_V34):
-    ...
+class MyHDF5Plugin(FileStoreHDF5IterativeWrite, HDF5Plugin_V34): ...
 
 
 class MyDetector(SingleTrigger, AreaDetector):
@@ -32,96 +31,24 @@ class MyDetector(SingleTrigger, AreaDetector):
     hdf1 = ADComponent(
         MyHDF5Plugin,
         "HDF:",
-        write_path_template ="/home/brw82791/out/%Y/%m/%d/",
+        write_path_template="/home/brw82791/out/%Y/%m/%d/",
         read_path_template="/home/brw82791/adOut/%Y/%m/%d/",
-
-     )
+    )
 
 
 class MyLaser(Device):
-    # power = Component(EpicsSignalRO, "laser:power")
-    pulse_id = Component(EpicsSignalRO, "TA1:PULSE_ID", name="pulse_id")
-
-
-#   Legacy laser variables for when laser is not set by pulse-id-gen
-#    pulse_id = Component(EpicsSignalRO, "pulse_id")
-#    freq = Component(EpicsSignalRO, "freq", kind="config")
-
-
-# Heavily influenced by _wait_for_value function in epics_pvs.py, does block
-def wait_for_value(signal: EpicsSignal, value, poll_time=0.01, timeout=10):
-    expiration_time = ttime.time() + timeout
-    current_value = signal.get()
-    while current_value != value:
-        # ttime.sleep(poll_time)
-        yield from bps.sleep(poll_time)
-        if ttime.time() > expiration_time:
-            raise TimeoutError(
-                "Timed out waiting for %r to take value %r after %r seconds"
-                % (signal, value, timeout)
-            )
-        current_value = signal.get()
-
-
-# Custom plan to move motor and then wait for laser pulse to take reading
-def pulse_sync(detectors, motor, laser, start, stop, steps):
-    step_size = (stop - start) / (steps - 1)
-
-    for det in detectors:
-        yield from bps.stage(det)
-
-    yield from bps.open_run()
-    for i in range(steps):
-        yield from bps.checkpoint()  # allows pausing/rewinding
-        yield from mv(motor, start + i * step_size)
-        currentPulse = laser.pulse_id.get()
-        yield from wait_for_value(
-            laser.pulse_id, currentPulse + 1, poll_time=0.001, timeout=10
-        )
-        yield from bps.trigger_and_read(list(detectors) + [motor] + [laser])
-    yield from bps.close_run()
-
-    for det in detectors:
-        yield from bps.unstage(det)
-
-
-# Custom plan to move motor based on detector status
-# designed for when detector is being triggered outside of bluesky
-def passive_scan(detectors, motor, start, stop, steps, adStatus, pulse_ID):
-    step_size = (stop - start) / (steps - 1)
-
-    yield from mv(motor, start)  # Move motor to starting position since may take time
-
-    yield from bps.open_run()
-
-    for det in detectors:
-        yield from bps.stage(det)
-
-    for i in range(steps):
-        yield from mv(motor, start + i * step_size)
-        yield from bps.checkpoint()
-        yield from wait_for_value(adStatus, 2, poll_time=0.001, timeout=10)
-        yield from bps.trigger_and_read([motor] + [pulse_ID])
-        yield from wait_for_value(adStatus, 0, poll_time=0.001, timeout=10)
-
-    for det in detectors:
-        yield from bps.unstage(det)
-
-    yield from bps.close_run()
+    pulse_id = Component(EpicsSignalRO, "TA1:PULSE_ID", name="pulse_id", kind="hinted")
 
 
 prefix = "TA1:CT_CAM:"
 det = MyDetector(prefix, name="det")
 det.hdf1.create_directory.put(-5)
-#det.hdf1.warmup()
 
 det.hdf1.kind = 3  # config | normal, required to include images in run documents
 
 det.cam.stage_sigs["image_mode"] = "Multiple"
 det.cam.stage_sigs["acquire_time"] = 0.005
 det.cam.stage_sigs["num_images"] = 1
-
-#det.hdf1.warmup()
 
 motor1 = EpicsMotor("TA1:SMC100:m1", name="motor1")
 
@@ -135,10 +62,7 @@ pulse_ID = EpicsSignalRO("TA1:PULSE_ID", name="pulse_ID")
 RE = RunEngine()
 
 bec = BestEffortCallback()
-# db = Broker.named("temp")  # This creates a temporary database
-# db = Broker.named("mongo")  # Connects to MongoDB database
-catalog = databroker.catalog["mongo"]
-#catalog = databroker.temp().v2
+catalog = databroker.catalog["mongo"]  # Connects to MongoDB database
 
 # Send all metadata/data captured to the BestEffortCallback.
 RE.subscribe(bec)
@@ -146,9 +70,8 @@ RE.subscribe(bec)
 RE.subscribe(catalog.v1.insert)
 
 
-# Examples of how to run both scans:
-# uids = RE(pulse_sync([det], motor1, laser1, -10, 10, 11))
-# uids = RE(passive_scan([det], motor1, -10, 10, 11, adStatus , pulse_ID))
+# Example of how to run a scan between 0 and 180 in 5 steps:
+# RE(scan([det,laser1], motor1, 0, 180, 5))
 
 # Take a look at the data from the run
 # run = catalog.v2[uids[0]]
